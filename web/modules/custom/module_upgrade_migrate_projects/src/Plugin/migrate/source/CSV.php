0ed77f33a97277d2f4a0e39e7b075ef3154c5d14
Allan Chappell <allan@fourkitchens.com>
2016-11-24 00:18:42 -0600

    updating migration to update terms when looking for keys.

--- web/modules/custom/module_upgrade_migrate_projects/src/Plugin/migrate/source/CSV.php
+++ web/modules/custom/module_upgrade_migrate_projects/src/Plugin/migrate/source/CSV.php
@@ -4,6 +4,7 @@ namespace Drupal\module_upgrade_migrate_projects\Plugin\migrate\source;
 
 use Drupal\migrate_source_csv\Plugin\migrate\source\CSV as CSVBase;
 use Drupal\migrate\Row;
+use Drupal\taxonomy\Entity\Term;
 
 /**
  * Source for CSV.
@@ -19,8 +20,22 @@ class CSV extends CSVBase {
    */
   public function prepareRow(Row $row) {
     $tids = \Drupal::entityQuery('taxonomy_term')
+      ->condition('vid', 'drupal_core_version')
+      ->condition('name', $row->api)
+      ->execute();
+    if (empty($tids)) {
+      $term = Term::create([
+        'vid' => 'drupal_core_version',
+        'name' => $row->api,
+      ]);
+      $term->save();
+      $tids[] = $term->id();
+    }
+    $drupal_core_version_tid = reset($tids);
+    $tids = \Drupal::entityQuery('taxonomy_term')
       ->condition('vid', 'project')
       ->condition('field_project_machine_name', $row->project_machine_name)
+      ->condition('field_drupal_core_version', $drupal_core_version_tid)
       ->execute();
     if (!empty($tids)) {
       $this->idMap->saveIdMapping($row, array(), MigrateIdMapInterface::STATUS_IGNORED);
